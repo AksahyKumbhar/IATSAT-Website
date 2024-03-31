d19d7bc463a8d2fe48425f2a162ed87786406926
AkshayKumbhar <86675450+AksahyKumbhar@users.noreply.github.com>
2024-03-31 14:10:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import MissionVisionGoal from "./components/MissionVisionGoal";
+import PrimaryMissions from "./components/PrimaryMissions";
+import Navbar from "./components/Navbar";
+import BannerContent from "./components/BannerContent";
+// import RegistrationForm from "./components/RegistrationForm";
+import Blog from "./components/Blog";
+import Carousel from "./components/Carousel";
+import FAQs from "./components/FAQs";
+import Footer from "./components/Footer";
+import ContactUs from "./components/ContactUs";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <BannerContent />
+      {/* <RegistrationForm /> */}
+      <br></br>
+      <br></br>
+      <hr></hr>
+      <MissionVisionGoal />
+      <br></br>
+      <br></br>
+      <hr></hr>
+      <PrimaryMissions />
+      <Blog />
+      <FAQs />
+      <ContactUs />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BannerContent.js
@@ -0,0 +1,309 @@
+// import React from "react";
+// import { Carousel, initTWE } from "tw-elements";
+// import CarouselComponent from "./Carousel";
+// import RegistrationForm from "./RegistrationForm";
+// import { Container, Row } from "react-bootstrap"; // Import Container and Row from react-bootstrap
+
+// initTWE({ Carousel });
+
+// function BannerContent() {
+//   return (
+//     <div>
+//       <div className="d-flex justify-content-between bg-blue-900 h-[460px] sm:h-[500px] w-[100vw] text-white flex flex-row mt-0" style={{height:'700px'}}>
+
+//         <div className="w-[100%] sm:flex md:mr-[10] gap-5 sm:mt-10 md:mt-10 sm:mr-40 md:mt-0 ml-20 mr-10 mt-20 justify-between items-center" style={{marginTop:"-250px"}}>
+//           <div className="sm:w-[60%] mb-6" >
+//             <div className="text-4xl">
+//               <b> Let's Join Hands Together To Bring The Revolution.</b>
+//             </div>
+//             <br />
+//             <div className="text-4xl sm:text-2xl text-blue-400 md:mb-10 sm:mb-4 mb-4 md:mb-1">
+//               Let's Bridge The Gap!
+//             </div>
+//             <span className="lg:text-3xl md:text-2xl sm:text-xl text-xl md:-mt-[60px] text-white-300 mb-20 pb-20 sm:mb-40 mt-20">
+//               Take your first class for{" "}
+//               <span className="lg:text-4xl md:text-3xl sm:text-xl text-3xl text-yellow-300">
+//                 FREE!
+//               </span>
+//             </span>
+//             <div className="mt-6">
+//               <a
+//                 href="#"
+//                 className="bg-red-500 h-[50px] w-[120px] z-[1] mt-4 rounded-md text-white text-center p-3 mt-4 shrink-0 break-keep"
+//               >
+//                 Enroll Now
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//       <section className="d-flex justify-content-between mt-0" style={{ marginTop: "-450px" }}>
+//         <div className="container">
+//           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+//             <Row>
+//               <RegistrationForm />
+//             </Row>
+//           </div>
+//         </div>
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default BannerContent;
+
+// import React from "react";
+// import { Carousel, initTWE } from "tw-elements";
+// import CarouselComponent from "./Carousel";
+// import RegistrationForm from "./RegistrationForm";
+// import { Container, Row } from "react-bootstrap"; // Import Container and Row from react-bootstrap
+
+// initTWE({ Carousel });
+
+// function BannerContent() {
+//   return (
+//     <div>
+//       <div className="d-flex justify-content-between bg-blue-900 h-[460px] sm:h-[500px] w-[100vw] text-white flex flex-row mt-0" style={{ height:'700px' }}>
+
+//         <div className=" d-flex justify-content-between w-[100%] sm:flex md:mr-[10] gap-5 sm:mt-10 md:mt-10 sm:mr-40 md:mt-0 ml-20 mr-10 mt-20 justify-between items-center" style={{ marginTop:"-250px" }}>
+//           <div className="d-flex justify-content-between align-item-center sm:w-[60%] mb-6" >
+//             <div className="text-4xl">
+//               <b> Let's Join Hands Together To Bring The Revolution.</b>
+//             </div>
+//             <br />
+//             <div className="text-4xl sm:text-2xl text-blue-400 md:mb-10 sm:mb-4 mb-4 md:mb-1">
+//               Let's Bridge The Gap!
+//             </div>
+//             <span className="lg:text-3xl md:text-2xl sm:text-xl text-xl md:-mt-[60px] text-white-300 mb-20 pb-20 sm:mb-40 mt-20">
+//               Take your first class for{" "}
+//               <span className="lg:text-4xl md:text-3xl sm:text-xl text-3xl text-yellow-300">
+//                 FREE!
+//               </span>
+//             </span>
+//             <div className="mt-6">
+//               <a
+//                 href="#"
+//                 className="bg-red-500 h-[50px] w-[120px] z-[1] mt-4 rounded-md text-white text-center p-3 mt-4 shrink-0 break-keep"
+//               >
+//                 Enroll Now
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//       <section className="mt-0 d-flex justify-content-between" style={{ marginTop:"-450px" }}>
+//         <Container className="d-flex justify-content-between">
+//           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+//             <Row>
+//               <RegistrationForm />
+//             </Row>
+//           </div>
+//         </Container>
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default BannerContent;
+
+// import React from "react";
+// import { Carousel, initTWE } from "tw-elements";
+// import CarouselComponent from "./Carousel";
+// import RegistrationForm from "./RegistrationForm";
+// import { Container, Row } from "react-bootstrap"; // Import Container and Row from react-bootstrap
+
+// initTWE({ Carousel });
+
+// function BannerContent() {
+//   return (
+//     <div className="d-flex justify-content-between" style={{ height: '900px' }}>
+//       <div className="d-flex justify-content-between bg-blue-900 h-[460px] sm:h-[500px] w-[100vw] text-white flex flex-row mt-0" style={{ height: '800px' }}>
+
+//         <div className="d-flex justify-content-between w-[100%] sm:flex md:mr-[10] gap-5 sm:mt-10 md:mt-10 sm:mr-40 md:mt-0 ml-20 mr-10 mt-20 justify-between items-center" style={{ height: '800px'}}>
+//           <div className="d-flex justify-content-between flex-column sm:w-[60%] mb-6" style={{ height: '800px',marginTop:'200px' }}>
+//             <div className="text-4xl">
+//               <b> Let's Join Hands Together To Bring The Revolution.</b>
+//             </div>
+//             <br />
+//             <div className="text-4xl sm:text-2xl text-blue-400 md:mb-10 sm:mb-4 mb-4 md:mb-1">
+//               Let's Bridge The Gap!
+//             </div>
+//             <span className="lg:text-3xl md:text-2xl sm:text-xl text-xl md:-mt-[60px] text-white-300 mb-20 pb-20 sm:mb-40 mt-20">
+//               Take your first class for{" "}
+//               <span className="lg:text-4xl md:text-3xl sm:text-xl text-3xl text-yellow-300">
+//                 FREE!
+//               </span>
+//             </span>
+//             <div className="mt-6">
+//               <a
+//                 href="#"
+//                 className="bg-red-500 h-[50px] w-[120px] z-[1] mt-4 rounded-md text-white text-center p-3 mt-4 shrink-0 break-keep"
+//               >
+//                 Enroll Now
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//       <section className="mt-0 d-flex justify-content-between" style={{ marginTop:"-450px", height: '800px' }}>
+//         <Container className="d-flex justify-content-between" style={{ height: '800px' }}>
+//           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+//             <Row>
+//               <RegistrationForm />
+//             </Row>
+//           </div>
+//         </Container>
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default BannerContent;
+
+
+// import React from "react";
+// import { Carousel, initTWE } from "tw-elements";
+// import CarouselComponent from "./Carousel";
+// import RegistrationForm from "./RegistrationForm";
+// import { Container, Row, Col } from "react-bootstrap"; // Import Container, Row, and Col from react-bootstrap
+
+// initTWE({ Carousel });
+
+// function BannerContent() {
+//   return (
+//     <Container fluid className="d-flex justify-content-between" >
+//       <Row className="d-flex justify-content-between bg-blue-900 h-[460px] sm:h-[500px] w-[100vw] text-white flex flex-row mt-0" style={{ height: '800px' }}>
+//         <Col className="d-flex justify-content-between w-[100%] sm:flex md:mr-[10] gap-5 sm:mt-10 md:mt-10 sm:mr-40 md:mt-0 ml-20 mr-10 mt-20 justify-between items-center" style={{ height: '800px'}}>
+//           <div className="d-flex justify-content-between flex-column sm:w-[60%] mb-6" style={{ height: '800px',marginTop:'200px' }}>
+//             <div className="text-4xl">
+//               <b> Let's Join Hands Together To Bring The Revolution.</b>
+//             </div>
+//             <br />
+//             <div className="text-4xl sm:text-2xl text-blue-400 md:mb-10 sm:mb-4 mb-4 md:mb-1">
+//               Let's Bridge The Gap!
+//             </div>
+//             <span className="lg:text-3xl md:text-2xl sm:text-xl text-xl md:-mt-[60px] text-white-300 mb-20 pb-20 sm:mb-40 mt-20">
+//               Take your first class for{" "}
+//               <span className="lg:text-4xl md:text-3xl sm:text-xl text-3xl text-yellow-300">
+//                 FREE!
+//               </span>
+//             </span>
+//             <div className="mt-6">
+//               <a
+//                 href="#"
+//                 className="bg-red-500 h-[50px] w-[120px] z-[1] mt-4 rounded-md text-white text-center p-3 mt-4 shrink-0 break-keep"
+//               >
+//                 Enroll Now
+//               </a>
+//             </div>
+//           </div>
+//         </Col>
+//       </Row>
+//       <Row className="mt-0 d-flex justify-content-between" style={{ marginTop:"-450px", height: '800px' }}>
+//         <Col className="d-flex justify-content-between" style={{ height: '800px' }}>
+//           <Row className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+//             <Col>
+//               <RegistrationForm />
+//             </Col>
+//           </Row>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// }
+
+// export default BannerContent;
+
+
+// import React from "react";
+// import { Carousel, initTWE } from "tw-elements";
+// import CarouselComponent from "./Carousel";
+// import RegistrationForm from "./RegistrationForm";
+// import { Container, Row, Col } from "react-bootstrap"; // Import Container, Row, and Col from react-bootstrap
+
+// initTWE({ Carousel });
+
+// function BannerContent() {
+//   return (
+//     <Container fluid className="d-flex justify-content-between" style={{ height: '900px' }}>
+//       <Row className="d-flex justify-content-between bg-blue-900 h-[460px] sm:h-[500px] w-[100vw] text-white flex flex-row mt-0" style={{ height: '800px' }}>
+//         <Col className="d-flex justify-content-between w-[100%] sm:flex md:mr-[10] gap-5 sm:mt-10 md:mt-10 sm:mr-40 md:mt-0 ml-20 mr-10 mt-20 justify-between items-center" style={{ height: '800px'}}>
+//           <div className="d-flex justify-content-between flex-column sm:w-[60%] mb-6" style={{ height: '800px',marginTop:'200px' }}>
+//             <div className="text-4xl">
+//               <b> Let's Join Hands Together To Bring The Revolution.</b>
+//             </div>
+//             <br />
+//             <div className="text-4xl sm:text-2xl text-blue-400 md:mb-10 sm:mb-4 mb-4 md:mb-1">
+//               Let's Bridge The Gap!
+//             </div>
+//             <span className="lg:text-3xl md:text-2xl sm:text-xl text-xl md:-mt-[60px] text-white-300 mb-20 pb-20 sm:mb-40 mt-20">
+//               Take your first class for{" "}
+//               <span className="lg:text-4xl md:text-3xl sm:text-xl text-3xl text-yellow-300">
+//                 FREE!
+//               </span>
+//             </span>
+//             <div className="mt-6">
+//               <a
+//                 href="#"
+//                 className="bg-red-500 h-[50px] w-[120px] z-[1] mt-4 rounded-md text-white text-center p-3 mt-4 shrink-0 break-keep"
+//               >
+//                 Enroll Now
+//               </a>
+//             </div>
+//           </div>
+//         </Col>
+//       </Row>
+//       <Row className="mt-8 d-flex justify-content-between flex-column-reverse flex-sm-row" style={{ marginTop:"-450px", height: '800px' }}>
+//         <Col className="d-flex justify-content-between" style={{ height: '800px' }}>
+//           <Row className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+//             <Col>
+//               <RegistrationForm />
+//             </Col>
+//           </Row>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// }
+
+// export default BannerContent;
+
+
+import React from "react";
+import { Carousel, initTWE } from "tw-elements";
+import CarouselComponent from "./Carousel";
+import RegistrationForm from "./RegistrationForm";
+import { Container, Row, Col } from "react-bootstrap"; // Import Container, Row, and Col from react-bootstrap
+
+initTWE({ Carousel });
+
+function BannerContent() {
+  return (
+    <Container fluid >
+      <Row className="bg-blue-900 text-white">
+        <Col sm={12} md={6} className="d-flex flex-column justify-content-center align-items-center " style={{ marginLeft: "100px," }}>
+          <div className="d-flex justify-content-between flex-column sm:w-[60%]   p-2 ml-5 mt-5 "  >
+            <h1 className="text-4xl font-bold mb-4 mt-6 ml-6">Let's Join Hands Together To Bring The Revolution.</h1>
+            <h2 className="text-2xl mb-4 mt-2 ml-6">Let's Bridge The Gap!</h2>
+            <p className="text-xl mb-8 ml-6">Take your first class for <span className="text-yellow-300">FREE!</span></p>
+            <a href="#" className=" d-flex flex-column justify-content-center align-items-center bg-red-500 text-white py-3 px-6 rounded-md ml-6">Enroll Now</a>
+          </div>
+        </Col>
+        <div className="d-flex justify-content-between mb-5" style={{marginTop:"40px"}}>
+          <div className="row ">
+            <div className="col-sm-12 col-md-6 " style={{ marginRight: "200px"}}>
+              <RegistrationForm />
+            </div>
+          </div>
+        </div>
+
+      </Row>
+    </Container>
+  );
+}
+
+export default BannerContent;
+
